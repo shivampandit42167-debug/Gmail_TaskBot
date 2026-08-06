b401f7ffe6198ebe856e1204c828257de81e14a0
shivampandit42167-debug <shivampandit42167@gmail.com>
2026-08-06 17:49:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ import time
 import os
 
 # --- CONFIGURATION ---
-TOKEN = '8717817143:AAEyVo1Brv7b9T-b8NE98E6iQflpR2HtWPI'
+TOKEN = '8683212510:AAEdE8kq5-5GuKerfPa_Mzaxovgb-J5VU4w'
 OWNER_ID = 8894779077  # Main Super Admin ID
 ADMIN_USERNAME = 'Raka_01'  
 
 # 👉 Tumhara Neon.tech PostgreSQL Database URL 👈
-DATABASE_URL = 'postgresql://neondb_owner:npg_vseV2Cxlwqi9@ep-mute-frost-axanqffq-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require' 
+DATABASE_URL = 'postgresql://neondb_owner:npg_TFXNmVEARt72@ep-twilight-sunset-axd07o2j-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require' 
 
 # Conversion Rate: 1 USDT = ₹94
 USDT_TO_INR_RATE = 94.0  
